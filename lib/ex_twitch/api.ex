fb30571676b05e06633d76a80b0fad7c02fe6b84
Kintel Williamson <malgasm@users.noreply.github.com>
2019-07-07 23:59:12 -0700

    add emojis endpoint and authorization for kraken endpoints

--- lib/ex_twitch/api.ex
+++ lib/ex_twitch/api.ex
@@ -3,7 +3,7 @@ defmodule ExTwitch.API do
 
   alias ExTwitch.TokenManager
 
-  plug Tesla.Middleware.BaseUrl, "https://api.twitch.tv/helix"
+  plug Tesla.Middleware.BaseUrl, "https://api.twitch.tv"
   plug Tesla.Middleware.JSON
 
   def users(opts) do
@@ -12,9 +12,18 @@ defmodule ExTwitch.API do
 
     data =
       read_token()
-      |> build_client
-      |> get("/users?" <> to_query_parameters(login_tuples ++ id_tuples))
+      |> build_client(:helix)
+      |> get("/helix/users?" <> to_query_parameters(login_tuples ++ id_tuples))
       |> data
+
+    {:ok, data}
+  end
+
+  def emotes() do
+    data =
+      read_token()
+      |> build_client(:kraken)
+      |> get("/kraken/chat/emoticons")
 
     {:ok, data}
   end
@@ -24,11 +33,20 @@ defmodule ExTwitch.API do
     token
   end
 
-  defp build_client(token) do
+  defp build_client(token, env) do
     Tesla.build_client [
-      {Tesla.Middleware.Headers, %{"Authorization" => "Bearer " <> token}}
+      {Tesla.Middleware.Headers, client_headers(env, token)}
     ]
   end
+
+  defp client_headers(:kraken, _) do
+    %{"Client-ID" => System.get_env("TWITCH_CLIENT_ID")}
+  end
+
+  defp client_headers(:helix, token) do
+    %{"Authorization" => "Bearer " <> token}
+  end
+
 
   defp data(%Tesla.Env{body: %{"data" => data}}), do: data
 
